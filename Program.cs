0481e2fe147f677ea9f50866978d1b81791bfb26
Oliver <oliverenciso07@gmail.com>
2025-01-29 20:44:59 +0800

    connectionstring

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using auth_project.Data;
 using auth_project.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("AuthStore");
+builder.Services.AddSqlite<AuthContext>(connectionString);
+
 var app = builder.Build();
 
 app.MapUserEndpoints();
