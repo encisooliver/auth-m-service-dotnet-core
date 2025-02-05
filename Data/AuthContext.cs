b81f808162abfad048adaf5180a7c851d5e114fa
Oliver <oliverenciso07@gmail.com>
2025-02-05 09:07:04 +0800

    Seeder user and usertype

--- Data/AuthContext.cs
+++ Data/AuthContext.cs
@@ -9,4 +9,17 @@ public class AuthContext(DbContextOptions<AuthContext> options)
 {
     public DbSet<User> Users => Set<User>();
     public DbSet<UserType> UserTypes => Set<UserType>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<UserType>().HasData(
+            new { Id = 1, Type = "Admin"},
+            new { Id = 2, Type = "User"}
+        );
+
+         modelBuilder.Entity<User>().HasData(
+            new { Id = 1, Name = "Administrator", UserName="admin", Email="admin@gmail.com", UserTypeId=1}
+        );
+    }
 }
