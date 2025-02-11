ce492ea05c6d6a28e272d9d87c81ecb24eeb3261
Oliver <oliverenciso07@gmail.com>
2025-02-11 21:02:32 +0800

    AddedScope IUserRepository, UserRepository

--- Program.cs
+++ Program.cs
@@ -1,14 +1,34 @@
 using auth_project.Data;
+using Microsoft.EntityFrameworkCore;
 using auth_project.Endpoints;
+using auth_project.Interfaces;
+using auth_project.Repositories;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var connectionString = builder.Configuration.GetConnectionString("AuthStore");
 builder.Services.AddSqlite<AuthContext>(connectionString);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapUserEndpoints();
-app.MigrateDb(); // from DataExtensions.cs
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+// app.MigrateDb(); // from DataExtensions.cs
 
 app.Run();
