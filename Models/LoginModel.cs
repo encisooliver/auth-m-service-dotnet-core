95f1e7e98ab48b58fba2940b12718965579ffdeb
Oliver <oliverenciso07@gmail.com>
2025-02-14 18:57:35 +0800

    login and registration model

---
+++ Models/LoginModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace auth_project.Models;
+
+public class LoginModel
+{
+    public string? username {get; set;}
+    public string? password {get; set;}
+}
---
+++ Models/RegistrationModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace auth_project.Models;
+
+public class RegistrationModel
+{
+    public String Name {get; set;}
+    public String UserName {get; set;}
+    public String Email {get; set;}
+}
